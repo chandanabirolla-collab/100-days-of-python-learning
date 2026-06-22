6c44342f26f5c93a40b7b3252bdd32e3a159eb39
Chandana <chandanabirolla@gmail.com>
2026-06-22 12:09:40 +0530

    Create day7.py
    using operators ,calculator

---
+++ day7.py
@@ -0,0 +1,12 @@
+n=8
+v=6
+ans1=n+v
+print("addition of ",n, "and",v,"is",ans1)
+ans2=n-v
+print("substraction of",n, "and",v,"is",ans2)
+print(n/v)
+print(n//v)
+print(n%v)
+ans3=(n*v)
+print("multliple of",n, "and",v,"is",ans3)
+print(n**2)
