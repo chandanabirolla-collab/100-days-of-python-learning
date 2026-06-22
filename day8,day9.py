3d460be0322416d168cbfa477ab640dfeed94fa1
Chandana <chandanabirolla@gmail.com>
2026-06-22 13:58:35 +0530

    Create day8,day9.py
    type casting....

---
+++ day8,day9.py
@@ -0,0 +1,25 @@
+n=8
+m=9.8
+print(type, n)
+print(type, m)
+b=n+m
+print(b)
+print(type, b)
+print(type(n))
+# type casting
+print(type(n))
+print(type(m))
+print(type(b))
+# type casting
+n="8.5"
+m=2.5
+b=float(n)
+print(type(n))
+print(type(m))
+print(type(b))
+c=m+b
+print(c)
+print(type(c))
+print(b)
+f=int(c)
+print(f)
