1ff8da8042caa37f075458ae07a1b06f07a446bd
Chandana <chandanabirolla@gmail.com>
2026-06-26 19:42:03 +0530

    11,12
    about strings and string slicing

---
+++ day11,12.py
@@ -0,0 +1,23 @@
+name="candy"
+print("hlo,"+ name)
+candy='''hey bro ,
+iam learning '''
+print(candy)
+print(name[0])
+#[]index we can get that letter 
+#using for loop now
+for character in candy:
+    print(character)
+#string slicing
+candy="rekha,sunitha" 
+print(candy[0:4])
+candy1="chandana"
+print(len(candy1))
+fj=len(candy)
+print(fj)
+print(candy[-7:-2  ])
+print(candy1[-7:-2])
+#it means -7 8-7=1 and 8-2=6 so it give output from index 1 to6 
+#it includes 1 not 6
+#len it says lenght 
+
