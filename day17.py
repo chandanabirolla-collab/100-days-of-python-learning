a9b8ff48a50431546f3bc95ba3a777b3f5c4bfa4
Chandana <chandanabirolla@gmail.com>
2026-06-29 15:54:54 +0530

    Create day17.py
    loops, for loop, range.

---
+++ day17.py
@@ -0,0 +1,13 @@
+#loops 
+#for loop
+colours=["red" , "orange" ,"white" , "green"]
+for color in colours:
+    print(color)
+    for i in color:
+      print(i)
+      #range 
+    
+for k in range(100):
+   print(k)
+for t in range(1 , 100, 10):
+   print(t)
