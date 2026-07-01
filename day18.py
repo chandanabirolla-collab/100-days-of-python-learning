4806d9bcb131822856effc6d93b93f92eb7d72bc
Chandana <chandanabirolla@gmail.com>
2026-07-01 11:01:04 +0530

    Create day18.py
    while loop

---
+++ day18.py
@@ -0,0 +1,18 @@
+#loops 
+#while loop
+number=8
+while(number<=20):
+ print(number)
+ number=number+2
+else:
+ print("stop")
+ count = 1
+
+# The loop will check this condition before every turn
+while count <= 5:
+    print(count)
+    
+    # CRITICAL STEP: Increase the count so the loop doesn't run forever
+    count += 1 
+
+print("Loop is finished!")
