645b634947b5f0cd5151f2e83f0dd42269c698cb
Chandana <chandanabirolla@gmail.com>
2026-07-01 16:57:51 +0530

    Create day21.py
    argument function

---
+++ day21.py
@@ -0,0 +1,7 @@
+#function arguments
+def learn_programming(name, topic):
+    # Joining strings the classic way
+    print(name + " is currently learning " + topic + "!")
+
+# Calling the function
+learn_programming("candy", "Python")
