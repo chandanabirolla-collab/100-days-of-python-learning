3676a6158d22f5fe3ffe7b8ec9daf3981e6166ca
Chandana <chandanabirolla@gmail.com>
2026-07-01 17:08:23 +0530

    Create day22.py
    #LIST

---
+++ day22.py
@@ -0,0 +1,15 @@
+#list
+l=[1,2,3,4,5,6,7,8]
+print(l)
+print(l[0])
+print(l[3])
+print(l[2])
+print(l[5])
+print(l[-2])
+print(l[1:4])
+num=[20, 40, 60, 80, 100]
+
+
+doubled = [x * 2 for x in num]
+
+print("New List:", doubled) # Output: [20, 40, 60, 80, 100]
