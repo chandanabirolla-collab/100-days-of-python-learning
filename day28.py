e47f6bde9dff00af07aac6985bbfca4d94ba06f8
Chandana <chandanabirolla@gmail.com>
2026-07-03 11:19:59 +0530

    Create day28.py
    f string

---
+++ day28.py
@@ -0,0 +1,11 @@
+letter = "Hey my name is {} and I am from {}"
+country = "India"
+name = "Candy"
+
+print(letter.format(name , country))
+print(f"Hey my name is {name} and I am from {country}")
+price = 49.09999
+txt = f"For only {price:.2f} dollars!"
+print(txt)
+# print(txt.format())
+print(type(f"{2 * 30}"))
