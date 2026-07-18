ae62cf409d0dd512799ddd785e728c961c3ff73f
Chandana <chandanabirolla@gmail.com>
2026-07-18 21:30:10 +0530

    Create day31.py
    introduction to sets

---
+++ day31.py
@@ -0,0 +1,15 @@
+#sets
+c={1,2,3,4,1,1,2,5,6}
+print(c)
+#empty set 
+candy=set()
+print(type(candy))
+#accessing sets 
+for values in c:
+    print(c)
+    print(values)
+frnd={"cvvf",4,5,6,3,4,"jghhkg"}
+for value in frnd:
+    print(value)
+    print(frnd)
+    
