31f37b68f43a62c1d62bf80e02571af411f48443
Chandana <chandanabirolla@gmail.com>
2026-08-02 17:04:29 +0530

    Create day35.py
    using else in for loop

---
+++ day35.py
@@ -0,0 +1,12 @@
+#for loop with else
+for x in range(5):
+    print("itteration no{}")
+    print(x)
+else:
+    print("byee")
+for x in range(8):
+    print(x)
+    if x==6:
+      break
+else:
+    print("byeeee")
