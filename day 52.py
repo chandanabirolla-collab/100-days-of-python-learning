850dab3136f277143c5c2ebc21e5d2adfa82608a
Chandana <chandanabirolla@gmail.com>
2026-09-20 13:16:56 +0000

    Lambda Functions

---
+++ day 52.py
@@ -0,0 +1,12 @@
+
+# Simple Lambda
+square = lambda x: x * x
+print(square(5))
+
+# Lambda with two arguments
+add = lambda a, b: a + b
+print(add(10, 20))
+
+# Lambda with condition
+check = lambda x: "Even" if x % 2 == 0 else "Odd"
+print(check(6))
