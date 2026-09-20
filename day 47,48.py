19525cff851ce001b3ad42598a84cfdc59c122dd
Chandana <chandanabirolla@gmail.com>
2026-09-20 10:09:21 +0000

    local variables and global variables

---
+++ day 47,48.py
@@ -0,0 +1,14 @@
+College="ABC"
+def branch():
+    class_name = "CSE"
+    print("College:", College)
+    print("Class:", class_name)
+branch() 
+name = "Chandana"   # Global variable
+
+def student():
+    age = 20         # Local variable
+    print("Name:", name)
+    print("Age:", age)
+
+student()
