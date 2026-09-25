bf59433a3ff18e51f561fe9dfca1a820cb8119c8
Chandana <chandanabirolla@gmail.com>
2026-09-25 07:23:31 +0530

    Create day54.py
    is vs ==

---
+++ day54.py
@@ -0,0 +1,27 @@
+#is vs ==
+
+a = 10
+b = 10
+
+print(a == b)
+print(a is b)
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+
+print(x == y)
+print(x is y)
+c=4
+r=8
+print(c==r)
+print(c is r)
+d=6
+g=6
+print(d==g)
+print(d is g)
+age = 20
+if age == 20:
+    print("You are 20")
+name = "candy"
+if name is "candy":
+    print(" candy name")
